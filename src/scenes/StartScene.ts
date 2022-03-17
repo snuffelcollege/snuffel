1a628155a8e1366fdb56b5b530108f544c858661
ruben <rubenvdb-work@outlook.com>
2022-03-17 15:41:36 +0100

    bugfix missing frame

--- src/scenes/StartScene.ts
+++ src/scenes/StartScene.ts
@@ -92,7 +92,7 @@ export default class StartScene extends Scene {
 			.setScale(0.5);
 
 		const startButton = this.add
-			.image(centerX, centerY+150, this.startButton, 1)
+			.image(centerX, centerY+150, this.startButton)
 			.setScale(0.5)
 			.setInteractive({ useHandCursor: true })
 			.on("pointerdown", () => {
