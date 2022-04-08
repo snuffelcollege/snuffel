093d519f0881320de6c150ed4ffafffc5a9e69a0
ruben <rubenvdb-work@outlook.com>
2022-04-08 11:51:32 +0200

    update to endscene

--- src/scenes/EndScene.ts
+++ src/scenes/EndScene.ts
@@ -1,6 +1,8 @@
 import BackgroundImage from "@assets/images/scenario_1/BG.png";
-import CongratsBadge from "@assets/images/congrats.png";
-import CongratsText from "@assets/images/congrats.png";
+import CongratsImage from "@assets/images/UI/congrats.png";
+import Option1 from "@assets/images/scenario_1/option_1.png";
+import Option2 from "@assets/images/scenario_1/option_2.png";
+import OptionStick from "@assets/images/world/option_stick.png";
 import { GameObjects, Scene, Time } from "phaser";
 import SceneLifecycle from "../SceneLifecycle";
 import { addFadeIn, fadeToBlack } from "../Utilities/Scene/Fader";
@@ -12,6 +14,7 @@ import SettingsConfig = Phaser.Types.Scenes.SettingsConfig;
 import Sprite = Phaser.GameObjects.Sprite;
 import SceneSong from "@assets/audio/scene.mp3";
 import CongratsAudio from "@assets/audio/congrats.mp3";
+import { Game } from "../Game";
 
 // Config for the scene defining gravity and debug settings.
 export const config: SettingsConfig = {
@@ -57,12 +60,21 @@ export default class Scene5 extends Scene implements SceneLifecycle {
 
 	private exitSceneKey!: string;
 
+	private option1!: string;
+
+	private option2!: string;
+
+	private optionStick!: string;
+
 	constructor(cfg: SettingsConfig = config) {
 		super(cfg);
 	}
 
 	public init(): void {
 		
+		this.option1 = "option1end";
+		this.option2 = "option2end";
+		this.optionStick = "stickend";
 
 		if (!WorldSceneConfig.key) {
 			throw Error("Exit scene key is undefined");
@@ -77,9 +89,11 @@ export default class Scene5 extends Scene implements SceneLifecycle {
 
 	// A key has to be unique for the entire project, not just this scene.
 	public preload(): void {
+		this.load.image(this.option1, Option1);
+		this.load.image(this.option2, Option2);
+		this.load.image(this.optionStick, OptionStick);	
 		this.load.image("backgroundEnd", BackgroundImage);
-		this.load.image("congratsBadge",CongratsBadge);
-		this.load.image("congratsText",CongratsText);
+		this.load.image("congratsImage",CongratsImage);
 		this.load.audio("sceneSong", SceneSong);
 		this.load.audio("congrats", CongratsAudio);		
 	}
@@ -92,6 +106,8 @@ export default class Scene5 extends Scene implements SceneLifecycle {
 
 		this.components.addComponent(img, MakeFullscreen);
 
+		this.add.image(950, 400,"congratsImage").setScale(0.7);
+
 		this.createSituation();
 	}
 
@@ -100,6 +116,118 @@ export default class Scene5 extends Scene implements SceneLifecycle {
 		this.sound.add("sceneSong", {volume: 0.3}).play({
 			loop: true
 		});
-		this.sound.add("congrats", {volume: 0.5}).play()
+		this.sound.add("congrats", {volume: 0.5}).play();
+	}
+	
+
+	private createChoice(): void {			
+		//create stick 1 and sign 1, add movecomponents
+		const stick1 = this.add.image(500,1280, this.optionStick);
+		const stick1move = this.components.addComponent(
+			stick1,
+			MoveTo
+		);
+		stick1move.setTarget({
+			x: stick1.x,
+			y: stick1.y - 300,
+		});		
+		stick1move.velocity = 280;
+		const button1 = this.add.image(500, 1200, this.option1);
+		button1.on("pointerover", () => {
+			this.game.sound.removeByKey("1startDialog2Audio");	
+			this.sound.add("1option1Audio", {volume: 1.5}).play();	
+			button1.angle = 5;			
+		});
+		button1.on('pointerout',() => {
+			this.game.sound.removeByKey("1option1Audio");	
+			button1.angle = 0;
+		})
+		const button1move = this.components.addComponent(
+			button1,
+			MoveTo
+		);
+		button1move.setTarget({
+			x: button1.x,
+			y: button1.y - 300,
+		});		
+		button1move.velocity = 280;
+		const stick2 = this.add.image(1000,1280, this.optionStick);
+		const stick2move = this.components.addComponent(
+			stick2,
+			MoveTo
+		);
+		
+		//create stick 2 and sign 2, add movecomponents
+		stick2move.setTarget({
+			x: stick2.x,
+			y: stick2.y - 300,
+		});		
+		stick2move.velocity = 280;
+		const button2 = this.add.image(1000, 1200, this.option2);
+		button2.on("pointerover", () => {
+			button2.angle = 5;	
+			this.game.sound.removeByKey("1startDialog2Audio");	
+			this.sound.add("1option2Audio", {volume: 1.5}).play();		
+		});
+		button2.on('pointerout',() => {
+			this.game.sound.removeByKey("1option2Audio");	
+			button2.angle = 0;
+		})
+		const button2move = this.components.addComponent(
+			button2,
+			MoveTo
+		);
+		button2move.setTarget({
+			x: button2.x,
+			y: button2.y - 300,
+		});		
+		button2move.velocity = 280;
+
+		//restart
+		button1
+			.setInteractive({ useHandCursor: true, pixelPerfect: true })
+			.on("pointerdown", () => {
+				//disables sign 1, moves signs and sticks of option 2 and 3 offscreen
+				button1.disableInteractive()				
+				button2move.setTarget({
+					x: button2.x,
+					y: button2.y + 300,
+				});		
+				button2move.velocity = 280;
+				stick2move.setTarget({
+					x: stick2.x,
+					y: stick2.y + 300,
+				});		
+				stick2move.velocity = 280;
+				
+			});
+
+		//continue
+		button2
+			.setInteractive({ useHandCursor: true, pixelPerfect: true })
+			.on("pointerdown", () => {
+				//disables sign 2, moves signs and sticks of option 1 and 3 offscreen
+				stick1move.setTarget({
+					x: stick1.x,
+					y: stick1.y + 300,
+				});		
+				stick1move.velocity = 280;
+				button1move.setTarget({
+					x: button1.x,
+					y: button1.y + 300,
+				});		
+				button1move.velocity = 280;
+				button2.disableInteractive()		
+				this.moveScene();
+			});
+	}
+	//fade to black and back to overworld
+	private moveScene() {
+		fadeToBlack(this, () => {
+			this.scene.stop(this.scene.key).wake(this.exitSceneKey);
+			this.scene.start("UIScene");
+		});
+		this.game.sound.removeByKey("sceneSong");
+		this.game.sound.resumeAll();
 	}
 }
